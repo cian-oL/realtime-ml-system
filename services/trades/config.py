05da2de060ec305f6ffe5580f60d41d5e1b9faba
cian-oL <cianoleary2010@gmail.com>
2025-01-13 17:28:48 +0000

    corrected model_config typo

--- services/trades/config.py
+++ services/trades/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
-    model_config: SettingsConfigDict(env_file=".env", encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", encoding="utf-8")
     kafka_broker_address: str
     kafka_topic: str
 
